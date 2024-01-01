b9adcf7b4185cea1527dd7891ffa4b23127d8ea4
HyuntaMansei <jchoi82kor@gmail.com>
2024-01-01 12:30:45 +0900

    some

--- bat/가로/run_all.bat
+++ bat/가로/run_all.bat
@@ -1,12 +1,14 @@
 chcp 65001
-start "" "1. ffbe-1.bat"
+
 start "" "1-노가장-8sd.bat"
-
-start "" "2. ffbe-2.bat"
+timeout /nobreak /t 3 >nul
 start "" "2-백수룡-zyl.bat"
-
-start "" "3. ffbe-3.bat"
+timeout /nobreak /t 3 >nul
 start "" "3-화산권마-AAP.bat"
-
-start "" "4. ffbe-4.bat"
+timeout /nobreak /t 3 >nul
 start "" "4-비류연-E0D.bat"
+timeout /nobreak /t 3 >nul
+start "" "1. ffbe-1.bat"
+start "" "2. ffbe-2.bat"
+start "" "3. ffbe-3.bat"
+start "" "4. ffbe-4.bat"
